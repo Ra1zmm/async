a893a30c0e8f1acade46cabb0bb0ece25dfc5556
Raizmm <raizmstudy@gmail.com>
2024-03-30 19:09:47 +0100

    first commit

---
+++ async.js
@@ -0,0 +1,9 @@
+// async function iterateWithAsyncAwait(x) {
+//     for (const num of x) {
+//         console.log(num)
+//         await new Promise(resolve => 
+//             setTimeout(resolve, 1000));
+//     }
+// }
+
+
